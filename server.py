41c171bf5c3caad7c1d1e596e1bdd7d19e8d87c4
7mile0n <144795540+7mile0n@users.noreply.github.com>
2024-01-25 21:14:13 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,1 @@
-from flask import Flask, request, render_template
-app = Flask(__name__)
 
-@app.route('/verify/{chatid}', methods=['POST'])
-async def verify(chatid):
-    client_ip = request.remote_addr
-    data = {'id': chatid, "ip": client_ip}
-    return render_template('index.html', data=data)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, ssl_context='adhoc', debug=False)
